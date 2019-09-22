74166296f69a35f30f5dee78382e68ca3c12c72f
mariaalc <marialcazar@correo.ugr.es>
2019-09-22 20:19:27 +0200

    Ruby: terminada clase mazosopresas || Java: nada

--- CivitasRuby/lib/mazo_sorpresas.rb
+++ CivitasRuby/lib/mazo_sorpresas.rb
@@ -31,15 +31,64 @@ module Civitas
       end
     end
     
-    #NO TERMINADO
+    
+    
     def siguiente
       if(!@barajada || @usadas==@sorpresas.size() && !@debug)
         @usadas=0
         @barajada = true
       end
       @usadas=@usadas+1
+      #Guardar la primera carta del array sorpresas en ultima_sorpresa
+      @ultima_sorpresa=@sorpresas[0]
       
+      #Quitar un elementos de un array en la posicion x es: delete_at(x)
+      @sorpresas.delete_at(0)
+      
+      #Añadir ultima_sorpresa al final del array sorpresas
+      @sorpresas << @ultima_sorpresa
+      
+      #Devolver referencia a esa carta
+      #HACER ESO ES DEVOLVER ULTIMA_SORPRESA?????????
+      return @ultima_sorpresa 
     end
+    
+    
+    
+    def inhabilitar_carta_especial(sorpresa)  
+      i=0
+      for i in @sorpresas
+        if(@sorpresas[i]==sorpresa)
+          @sorpresas.delete_at(i)
+          @cartas_especiales << sorpresa
+          @diario.ocurre_evento("Carta inhabilitada")
+        else
+          @diario.ocurre_evento("Carta no inhabilitada")
+        end
+      end
+    end
+    
+    
+    
+    def habilitar_carta_especial(sorpresa)
+      i=0
+      for i in @sorpresas
+        if(@sorpresas[i]==sorpresa)
+          #Así ya se añadiría al final, y no hay ningún problema, porque
+          #se borra depués, no  ??????????
+          @sorpresas << sorpresa
+          @sorpresas.delete_at(i)
+          @diario.ocurre_evento("Carta habilitada")
+        else
+          @diario.ocurre_evento("Carta no habilitada")
+        end
+      end
+    end
+    
+    
+    
+    
+    
     
   end
 end
