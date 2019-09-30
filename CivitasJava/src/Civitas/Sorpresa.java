ef2fe946f9258eb4323f6a0ae0d89142430af7ff
mariaalc <marialcazar@correo.ugr.es>
2019-09-30 14:11:48 +0200

    ruby y java emepzado Sorpresa

--- CivitasJava/src/Civitas/Sorpresa.java
+++ CivitasJava/src/Civitas/Sorpresa.java
@@ -1,12 +1,39 @@
 package Civitas;
+
 public class Sorpresa {
-    private String nombre;
+    private String texto;
+    private int valor;
+    private Tablero tablero;
+    private TipoSorpresa tipo;
+    private MazoSorpresas mazo;
     
-    Sorpresa(String nom){
-        nombre = nom;
+    //Constructor para la sorpresa que envía a la cárcel
+    Sorpresa(TipoSorpresa tipo, Tablero tablero){
+        this.tipo = tipo;
+        this.tablero = tablero;
     }
     
-    String getNombre(){
-        return nombre;
+    //Constructor para la sorpresa que envía al jugador a otra casilla
+    Sorpresa(TipoSorpresa tipo, Tablero tablero, int valor, String texto){
+        this.tipo = tipo;
+        this.tablero = tablero;
+        this.valor = valor;
+        this.texto = texto;    
     }
+    
+    //Constructor para el resto de sorpresas
+    Sorpresa(TipoSorpresa tipo, int valor, String texto){
+        this.tipo=tipo;
+        this.valor=valor;
+        this.texto=texto;
+    }
+    
+    //Constructor para sorpresa que permite evitar la carcel
+    Sorpresa(TipoSorpresa tipo, MazoSorpresas mazo){
+        this.tipo = tipo;
+        this.mazo = mazo;        
+    }
+    
+    
+  
 }
