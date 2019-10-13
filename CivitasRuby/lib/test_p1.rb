165f9482715fb1ddf6b74e5db99f447d11677af5
mariaalc <marialcazar@correo.ugr.es>
2019-10-14 00:08:59 +0200

    jeejee

--- CivitasRuby/lib/test_p1.rb
+++ CivitasRuby/lib/test_p1.rb
@@ -209,14 +209,10 @@ module Civitas
       
       puts "\n Esta encarcelado?"
       puts @@civitas.get_jugador_actual.encarcelado
-      
-<<<<<<< HEAD
+
       puts "\n Encarcelar al jugador"
       puts @@civitas.get_jugador_actual.encarcelar(@@civitas.tablero.num_casilla_carcel)
-=======
-      puts @@civitas.tablero.casillas[1].titulo.get_precio_venta
->>>>>>> f6c0aad362c59648ee8d129a5d939fc84d15f9ce
-      
+            
       puts "\n Esta encarcelado?"
       puts @@civitas.get_jugador_actual.encarcelado
       
@@ -242,6 +238,8 @@ module Civitas
       puts @@civitas.get_jugador_actual.pasa_por_salida
       
       
+      puts @@civitas.tablero.casillas[1].titulo.get_precio_venta
+
       
 
       
