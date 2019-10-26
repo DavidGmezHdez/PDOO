4650e75ba00fb63edddc06ea668fa636afb5ac99
mariaalc <marialcazar@correo.ugr.es>
2019-10-26 11:48:01 +0200

    vista en ruby hecha excepto dudas

--- CivitasRuby/lib/vista_textual.rb
+++ CivitasRuby/lib/vista_textual.rb
@@ -1,9 +1,20 @@
 #encoding:utf-8
 require_relative 'operaciones_juego'
 require 'io/console'
+require_relative 'salidas_carcel'
+require_relative 'respuestas'
+require_relative 'diario'
+
 
 module Civitas
   class Vista_textual
+    
+    def initialize
+      @juego_model = nil
+      @i_gestion = -1
+      @i_propiedad = -1
+    end
+    
     
     def mostrar_estado(estado)
       puts estado
@@ -16,6 +27,7 @@ module Civitas
       print "\n"
     end
 
+    
     def lee_entero(max,msg1,msg2)
       ok = false
       begin
@@ -60,32 +72,62 @@ module Civitas
 
     
     def comprar
+      opcion = menu("¿Deseas comprar la propiedad?",Respuestas::lista_respuestas)
+      return lista_respuestas[opcion]
     end
+    
 
     def gestionar
+      @i_gestion = menu("¿Qué gestion inmobiliaria deseas hacer?",NOSEEE)
+      
+      nombres_propiedades = Array.new
+      
+      for i in @juego_model.jugador_actual.propiedades.size()
+        nombres_propiedades << (@juego_model.jugador_actual.propiedades[i].nombre)
+      end
+      
+      @i_propiedad = menu("¿Qué propiedad deseas gestionar?",nombres_propiedades)
     end
+    
 
     def getGestion
+      return @i_gestion
     end
+    
 
     def getPropiedad
+      return @i_propiedad
     end
+    
 
     def mostrarSiguienteOperacion(operacion)
+      puts "Siguiente operacion" + operacion.to_s
     end
+    
 
     def mostrarEventos
+      while (Diario.instance.eventos_pendientes)
+        puts Diario.instance.leer_evento        
+      end
     end
+    
 
     def setCivitasJuego(civitas)
          @juegoModel=civitas
          self.actualizarVista
     end
+    
 
     def actualizarVista
+      puts @juegoModel.info_jugador_texto
     end
-
     
+    
+    def salir_carcel
+      opcion = menu("Elige la forma para intentar salir de la carcel", 
+        SalidasCarcel::lista_salidas_carcel)
+      return lista_salidas_carcel[opcion]
+    end
+        
   end
-
 end
