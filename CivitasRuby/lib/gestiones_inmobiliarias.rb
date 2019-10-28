c59887709e4643aef8d72ce4f187f3881bea4bfe
mariaalc <marialcazar@correo.ugr.es>
2019-10-28 12:58:32 +0100

    vista y modelo terminados en ruby

--- CivitasRuby/lib/gestiones_inmobiliarias.rb
+++ CivitasRuby/lib/gestiones_inmobiliarias.rb
@@ -1,17 +1,8 @@
 # encoding:utf-8
 
 module Civitas
-  class GestionesInmobiliarias
-    VENDER = :vender
-    HIPOTECAR = :hipotecar
-    CANCELAR_HIPOTECA = :cancelar_hipoteca
-    CONSTRUIR_CASA = :construir_casa
-    CONSTRUIR_HOTEL = :contruir_hotel
-    TERMINAR = :terminar  
-    
-    lista_gestiones_inmobiliarias = [OperacionesInmobiliarias::VENDER,
-      OperacionesInmobiliarias::HIPOTECAR,OperacionesInmobiliarias::CANCELAR_HIPOTECA,
-      OperacionesInmobiliarias::CONSTRUIR_CASA,OperacionesInmobiliarias::CONSTRUIR_HOTEL,
-      OperacionesInmobiliarias::TERMINAR]
+  class GestionesInmobiliarias    
+    lista_gestiones_inmobiliarias = [:VENDER, :HIPOTECAR, :CANCELAR_HIPOTECA, 
+      :CONSTRUIR_CASA, :CONSTRUIR_HOTEL, :TERMINAR]
   end
 end
--- CivitasRuby/lib/salidas_carcel.rb
+++ CivitasRuby/lib/salidas_carcel.rb
@@ -1,9 +1,6 @@
 # encoding:utf-8
 module Civitas
   class SalidasCarcel
-    PAGANDO = :pagando
-    TIRANDO = :tirando
-    
-    lista_salidas_carcel = [SalidasCarcel::PAGANDO,SalidasCarcel::TIRANDO]
+    lista_salidas_carcel = [:PAGANDO, :TIRANDO]
   end
 end
