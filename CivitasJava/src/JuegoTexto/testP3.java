3a184ad8d12a5f7b99a576896221acfba01a8417
mariaalc <marialcazar@correo.ugr.es>
2019-11-09 20:28:01 +0100

    javaaa

--- CivitasJava/src/JuegoTexto/testP3.java
+++ CivitasJava/src/JuegoTexto/testP3.java
@@ -9,17 +9,30 @@ public class testP3 {
     private static Dado dado = Dado.getInstance();
     private static final Scanner in = new Scanner (System.in);
     
+    private static ArrayList<String> getNombreJugadores(){
+        ArrayList<String> lista = new ArrayList<>();
+        int num_jugadores;
+
+        System.out.println("Introduce el numero de jugadores:");
+        num_jugadores = in.nextInt();
+
+        for(int i=0; i<num_jugadores; i++){
+            System.out.println("Introduce el nombre del jugador:");
+            String s = in.next();
+        lista.add(s);
+        }
+
+        return lista;
+    }
+    
     public static void main(String[] args) {
         ArrayList<String> nombres = new ArrayList<>();
-        nombres.add("Pepe");
-        nombres.add("Carla");
-        /*
-        dado.setDebug(false);
-        System.out.println("Debug: " + dado.isDebug());
-        */
+        nombres = getNombreJugadores();
+        
         CivitasJuego civitas = new CivitasJuego(nombres);
         VistaTextual vista = new VistaTextual();
         civitas.setDebugDado(true);
+        
         Controlador controlador = new Controlador(civitas,vista);
         controlador.juega();
         
